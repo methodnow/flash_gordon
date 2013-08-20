61cc133ede6e0ac35b94eb5cb9338877a26a404c
Ernest Sim <ernest@anzu.sg>
2013-08-20 15:20:54 +0800

    Changed ridiculous tab spacing, added updated README and added gitignore

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ flash_gordon
 ============
 
 Flash Gordon is a Rails Flash helper Gem that allows you to add custom flash notification handlers anywhere within your application.
+
+Installation:
+-------------
+
+```ruby
+  # Gemfile
+  gem 'flash_gordon'
+```
+
+Usage:
+------
+
+To add a custom flash event user the append method, the default zone will be :alert
+
+```ruby
+  FlashGordon::Flash.append("Notification Message")
+```
+
+You can specify a zone in the method, right now zones are hardcoded to the follow. 
+This is purely for bootstrap elements to render the correct colored alert div's
+
+```ruby
+  # Available Zones are :warning, :alert, :log, :success, :info, :error, :danger
+  FlashGordon::Flash.append("Success Message", :success)
+``` 
+To render the flashes, just put this anywhere in your view where you want to show, e.g application.html.erb
+
+```ruby
+  # application.html.erb
+  <%= render_flash %>
+```
--- lib/flash_gordon/flash.rb
+++ lib/flash_gordon/flash.rb
@@ -1,38 +1,38 @@
 module FlashGordon
-	class Flash
+  class Flash
     # Messages fall under different message zones, for now zones are hardcoded
     # eventually we can add custom ones via initializers
 
-		@messages = {:error => [], :alert => [], :info => [], :log => [], :success => [], :warning => [], :danger => []}
+    @messages = {:error => [], :alert => [], :info => [], :log => [], :success => [], :warning => [], :danger => []}
 
-		def self.append(message,zone=:warning, options={})
-			# Added options for future use
-			if @messages[zone]
-				if message.class == Array
-					message.each do |m|
-						@messages[zone] << m
-					end
-				else
-					@messages[zone] << message
-				end
-			else
-				raise "Zone does not exist, please use the following zones - :error, :info, :log, :success, :warning, :alert"
-			end
-		end
+    def self.append(message,zone=:warning, options={})
+      # Added options for future use
+      if @messages[zone]
+        if message.class == Array
+          message.each do |m|
+            @messages[zone] << m
+          end
+        else
+          @messages[zone] << message
+        end
+      else
+        raise "Zone does not exist, please use the following zones - :error, :info, :log, :success, :warning, :alert"
+      end
+    end
 
-		def self.render(zone=:all, glue="\n")
-			if zone == :all
-				@messages.each_pair do |k,v|
-					@messages[k] = v.join(glue) unless v.empty?
-				end
-				@messages
-			elsif @messages[zone]
-				@messages[zone].join(glue)
-			end
-		end
+    def self.render(zone=:all, glue="\n")
+      if zone == :all
+        @messages.each_pair do |k,v|
+          @messages[k] = v.join(glue) unless v.empty?
+        end
+        @messages
+      elsif @messages[zone]
+        @messages[zone].join(glue)
+      end
+    end
 
-		def self.reset
-			@messages = {:error => [], :alert => [], :info => [], :log => [], :success => [], :warning => [], :danger => []}
-		end
-	end
+    def self.reset
+      @messages = {:error => [], :alert => [], :info => [], :log => [], :success => [], :warning => [], :danger => []}
+    end
+  end
 end

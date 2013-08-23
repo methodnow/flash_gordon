833ad9650c881ac95a0df07118a50fcedd20849b
Ernest Sim <ernest@anzu.sg>
2013-08-23 10:48:41 +0800

    Minor refactor to flash_helper

--- lib/flash_gordon/flash_helper.rb
+++ lib/flash_gordon/flash_helper.rb
@@ -2,13 +2,7 @@ module FlashGordon
   module FlashHelper
     def render_flash
       FlashGordon::Flash.render.each_pair do |k,v|
-        unless v.empty?
-          unless flash.now[k]
-            flash.now[k] = v unless v.empty?
-          else
-            flash.now[k] << ("<br/>" + v) unless v.empty?
-          end
-        end
+          (flash.now[k] ? (flash.now[k] << ("<br/>" + v)) : (flash.now[k] = v)) unless v.empty?
       end
       FlashGordon::Flash.reset
       flash.map{|f| "<div class='alert alert-#{f[0]}'>#{f[1]}</div>"}.join.html_safe
